6f60b36065a489021c468e9942e828bfd0c7135b
Chestnut-TH <t.hirai.1997.0328.chestnut@gmail.com>
2022-04-25 00:18:35 +0900

    first commit

---
+++ docker_spring/src/test/java/docker_spring/docker_spring/DockerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package docker_spring.docker_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
